1c1d96168f1d660249d8e0cffbfb802ff4617646
Piotrek Rybiec <piotrrybiec28@gmail.com>
2023-01-17 16:07:34 +0100

    utils lib

--- lib/utils.go
+++ lib/utils.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "os"
+  "bufio"
   "ioutil"
   "strconv"
 )
@@ -15,6 +16,27 @@ func check(e error) {
 
 
 /* *** FILES *** */
+
+func file_to_lines(path string) (lines []string) {
+  const MAX_CAPACITY = 1000000000 // max line length
+
+  f, err := os.Open(path)
+  check(err)
+  defer f.Close() // defer task
+
+  scanner := bufio.NewScanner(f) 
+  buf := make([]byte, MAX_CAPACITY)
+  scanner.Buffer(buf, MAX_CAPACITY)
+
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+
+  err = scanner.Err()
+  check(err)
+
+  return lines
+}
 
 func file_to_string(fn string) string {
   bytes, err := ioutil.ReadFile(fn)
