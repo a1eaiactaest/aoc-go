e728ee45d48b15c9617ca8bd5202df8d000ce7b2
Piotrek Rybiec <piotrrybiec28@gmail.com>
2023-01-17 16:07:24 +0100

    day1

--- l1/main.go
+++ l1/main.go
@@ -1,51 +1,32 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-)
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func read_file(path string) (lines []string) {
-	f, err := os.Open(path)
-	check(err)
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	const MAX_CAPACITY = 10000000
-	buf := make([]byte, MAX_CAPACITY)
-	scanner.Buffer(buf, MAX_CAPACITY)
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-	check(err)
-
-	return lines
-}
+import "fmt"
 
 func part1(lines []string) int {
-	acc := 0
-	mcal := 0
+  cals := 0
+	max_cals := 0
 
 	for _, line := range lines {
-		acc += strconv.Atoi(line)
-	}
-
-	return mcal
+    if line == "" {
+      if cals > max_cals { // comp
+        max_cals = cals 
+      }
+      cals = 0
+    } else { // acc
+      cals += atoi(line)
+    }
+	} 
+	return max_cals
 }
 
+/*
+func part2(lines []string) int {
+  return lines
+}
+*/
+
 func main() {
-	lines := read_file("input.txt")
+	lines := file_to_lines("input.txt")
 	p1 := part1(lines)
 	fmt.Println(p1)
 }
